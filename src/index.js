00354500f448d8d8b97fbcf0146e36a7ddc7e003
Yuri Zinchenko <zinchen777@gmail.com>
2018-02-24 21:47:03 +0300

    add alphavantage, delete demo data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import alphavantage from 'alphavantage'
+
+console.log(process.env);
+
+const alpha = alphavantage({
+  key: 'WJY2N6Q86IFIARMN'
+})
+
+alpha.data.batch([`msft`, `aapl`]).then(data => {
+  console.log(data);
+});
+
+
+alpha.data.intraday(`msft`).then(data => {
+  console.log(data);
+});
+
+
+alpha.data.intraday(`msfbadt`)
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
+ReactDOM.render(
+  <div>App</div>, document.getElementById('root')
+)
